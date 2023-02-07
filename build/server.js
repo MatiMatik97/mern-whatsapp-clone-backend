ccf671c9851d0c80eaa40463c49ac9c122d9393c
MatiMatik97 <matimatik97@gmail.com>
2023-02-07 19:38:57 +0100

    🗳 DEV-011 - CORS

--- build/server.js
+++ build/server.js
@@ -22,7 +22,10 @@ const pusherOptions = {
 const pusher = new Pusher(pusherOptions);
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200,
+}));
 // db config
 const db_username = process.env.DB_USERNAME;
 const db_passowrd = process.env.DB_PASSOWRD;
